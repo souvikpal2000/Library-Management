6dcbd0f7b99c91bc11490909103a9a69655d8c63
Sumit-bera <berasumit956@gmail.com>
2021-06-04 21:44:38 +0530

    2nd Commit

--- SearchUser.php
+++ SearchUser.php
@@ -11,7 +11,8 @@
 		<!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>-->
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+		-->
 
 		<script src="https://kit.fontawesome.com/68b0726c11.js" crossorigin="anonymous"></script>
 
